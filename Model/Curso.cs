3ce6a1d14337670e62b518357884ab11ca823613
Rodrigo Moro <rodrigo.moro.francisco@outlook.com>
2024-04-05 16:06:59 -0300

    Adicionar arquivos de projeto.

---
+++ Model/Curso.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCAapp2.Model
+{
+    public class Curso
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        private List<Aluno> alunos = new List<Aluno>();
+        private List<Disciplina> disciplinas = new List<Disciplina>();
+
+        public void AddAluno(Aluno aluno)
+        {
+            alunos.Add(aluno);
+        }
+
+        public string ExibirAlunos()
+        {
+            string result = $"Alunos do Curso {Nome}:\n";
+            foreach (var aluno in alunos)
+            {
+                result += $"{aluno.Nome}\n";
+            }
+            return result;
+        }
+
+        public void AddDisciplina(Disciplina disciplina)
+        {
+            disciplinas.Add(disciplina);
+        }
+
+        public string ExibirGrade()
+        {
+            string result = $"Grade do Curso {Nome}:\n";
+            foreach (var disciplina in disciplinas)
+            {
+                result += $"{disciplina.Nome}\n";
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using SCAapp2.Model;
+
+class Program
+{
+    static void Main()
+    {
+        // Criando instâncias das classes
+        Aluno aluno1 = new Aluno { Matricula = 1, Nome = "Alice" };
+        Aluno aluno2 = new Aluno { Matricula = 2, Nome = "Bob" };
+        Disciplina disciplina1 = new Disciplina { Codigo = 101, Nome = "Matemática" };
+        Disciplina disciplina2 = new Disciplina { Codigo = 102, Nome = "História" };
+
+        Curso curso1 = new Curso { Codigo = 1, Nome = "Engenharia" };
+        curso1.AddAluno(aluno1);
+        curso1.AddAluno(aluno2);
+        curso1.AddDisciplina(disciplina1);
+        curso1.AddDisciplina(disciplina2);
+
+        // Exibindo informações do curso
+        Console.WriteLine($"Curso: {curso1.Nome}, Código: {curso1.Codigo}");
+        Console.WriteLine(curso1.ExibirAlunos());
+        Console.WriteLine(curso1.ExibirGrade());
+    }
+}
